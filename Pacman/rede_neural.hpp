b4053ef265c73df049639a6d7012c1b3bee6713e
Dr-Verdin <piscioneri24122004@gmail.com>
2024-12-22 00:40:06 -0300

    completo

--- Pacman/rede_neural.hpp
+++ Pacman/rede_neural.hpp
@@ -1,36 +1,36 @@
 #ifndef REDE_NEURAL_HPP
-#define REDE_NEURAL_HPP
+    #define REDE_NEURAL_HPP
 
-#include <vector>
-#include <iostream>
-#include <cstdlib>  
-#include <ctime>    
-#include "feedforward.hpp"
-using namespace std;
+    #include <vector>
+    #include <iostream>
+    #include <cstdlib>  
+    #include <ctime>    
+    #include "feedforward.hpp"
+    using namespace std;
 
-class RedeNeural {
-private:
-    vector<FeedForward> camadas;  // Vetor de objetos FeedForward, uma para cada camada
-    vector<int> camadas_int;
-public:
-    // Construtor antigo que recebe diretamente um vetor de FeedForwards (camadas)
-    RedeNeural(const std::vector<FeedForward>& camadas_iniciais);
+    class RedeNeural {
+    private:
+        vector<FeedForward> camadas;  // Vetor de objetos FeedForward, uma para cada camada
+        vector<int> camadas_int;
+    public:
+        // Construtor antigo que recebe diretamente um vetor de FeedForwards (camadas)
+        RedeNeural(const std::vector<FeedForward>& camadas_iniciais);
 
-    // Construtor que recebe um vetor de FeedForwards (cada camada)
-    RedeNeural(const std::vector<int>& numNeuronsPorCamada);
+        // Construtor que recebe um vetor de FeedForwards (cada camada)
+        RedeNeural(const std::vector<int>& numNeuronsPorCamada);
 
-    RedeNeural(const RedeNeural &rede_copiar);
+        RedeNeural(const RedeNeural &rede_copiar);
 
-    // Função de feedforward completo, passando X por todas as camadas
-    vector<double> feedForwardCompleto(const std::vector<double>& X);
+        // Função de feedforward completo, passando X por todas as camadas
+        vector<double> feedForwardCompleto(const std::vector<double>& X);
 
-    // Função para crossover entre duas redes neurais
-    static RedeNeural crossover(const RedeNeural& redeA, const RedeNeural& redeB);
+        // Função para crossover entre duas redes neurais
+        static RedeNeural crossover(const RedeNeural& redeA, const RedeNeural& redeB);
 
-    // Função para aplicar mutação em toda a rede neural
-    void mutar(double mutation_factor);
+        // Função para aplicar mutação em toda a rede neural
+        void mutar(double mutation_factor);
 
-    std::vector<int> getLayers();
-};
+        std::vector<int> getLayers();
+    };
 
 #endif // REDE_NEURAL_HPP
